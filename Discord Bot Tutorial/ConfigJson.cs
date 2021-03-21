3a69398d274e0f5c3e55f1c2a8d87f7e30f23cf9
Drew Fitzpatrick <dfitzpatrick@bignerdranch.com>
2021-03-21 14:49:40 -0400

    Get bot up and running.

---
+++ Discord Bot Tutorial/ConfigJson.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+
+namespace Discord_Bot_Tutorial
+{
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
